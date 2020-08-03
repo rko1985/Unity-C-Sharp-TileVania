1f8ab9eb1e357aa7bd7a00324831a2f9a3d56295
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-08-03 09:42:01 -1000

    make player move horizontally with keyboard input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal"); // value is between -1 and +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
