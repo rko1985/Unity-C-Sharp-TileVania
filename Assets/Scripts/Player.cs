8c9f2ef3b7502d9935e17d5a8696cfae3b040183
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-08-03 09:51:44 -1000

    flip player sprite when changing direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -24,5 +25,16 @@ public class Player : MonoBehaviour
         float controlThrow = Input.GetAxis("Horizontal"); // value is between -1 and +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        //if the player is moving horizontally
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed)
+        {
+            //reverse the current scaling of x axis
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
