36fb0a814bf83f4c6caec2a2465fdca1612041d2
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-08-25 07:05:38 -1000

    persistent lives and score

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -6,9 +6,11 @@ public class CoinPickup : MonoBehaviour
 {
 
     [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
         AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
+    [SerializeField] int score = 0;
+
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
 
     private void Awake()
     {
@@ -25,7 +30,14 @@ public class GameSession : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-                
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -45,6 +57,7 @@ public class GameSession : MonoBehaviour
         playerLives--;
         var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString();
     }
 
     private void ResetGameSession()
